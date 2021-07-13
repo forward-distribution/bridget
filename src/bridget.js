49babf3dd3b1b1ded271a08a98998a1672d9bda3
Ivan Sekovanikj <Ivan.Sekovanikj@netcetera.com>
2021-07-13 19:55:23 +0200

    Implement navigation mechanism

--- src/bridget.js
+++ src/bridget.js
@@ -1,53 +1,66 @@
 import bridge from './bridge/bridge.js'
 
+function isInternalLink (url, host) {
+  return url.host === host
+}
+
+// This is a placeholder check for testing purposes on BMG. It would ideally
+// be replaced by a client-side regex detailing what we should look for in an
+// article.
+
+function isArticle (url) {
+  const re = new RegExp('^[a-zA-Z-]+-[0-9]+$', 'g')
+  return re.test(url.split('/').pop())
+}
+
+const navigationObjectResolver = href => {
+  const location = window.location
+  const constructedUrl = new URL(href, location.origin)
+  const pathname = constructedUrl.pathname
+
+  if (isInternalLink(constructedUrl, location.host)) {
+    if (isArticle(pathname)) {
+      return { type: 'article', url: pathname }
+    }
+    return pathname === '/' ? { type: 'startpage' } : { type: 'page', url: pathname }
+  }
+
+  return { type: 'external', url: href }
+}
+
 const linkingHandler = event => {
   event.preventDefault()
   const element = event.target.closest('a')
-  // alert('Found anchor tag ! :D')
-  console.log('ISE: ', element)
+  const navigationObject = navigationObjectResolver(element.href)
+  const { type, url } = navigationObject
 
-  bridge.navigateExternally(element.href)
-  // abortController.abort()
+  switch (type) {
+    case 'article': {
+      bridge.navigateToArticle(url)
+      break
+    }
+    case 'page': {
+      bridge.navigateToPage(url)
+      break
+    }
+    case 'startpage': {
+      bridge.navigateToStartpage()
+      break
+    }
+    case 'external': {
+      bridge.navigateExternally(url)
+      break
+    }
+  }
 }
 
 if (window.ReactNativeWebView) {
-  // document.addEventListener('DOMContentLoaded', () => {
-  //   // alert('DOM loaded successfully !')
-  //   const anchors = document.querySelectorAll('a')
-  //   console.log('ISE: ', anchors)
-  //   Array.from(anchors).map(a => a.addEventListener('click', e => {
-  //     alert('Caught on click event !')
-  //     alert(e)
-  //   }))
-  // })
   document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('click', e => {
-      // alert('A click event has been invoked ! :O')
-      // is anchor
-      // console.log('ISE: event ', e)
-      // console.log('ISE: target ', e.target)
-      // console.log('ISE: eventPhase', e.eventPhase)
-      // console.log('ISE: currentTarget', e.currentTarget)
-      // console.log('ISE: fromElement', e.fromElement)
-      // console.log('ISE: srcElement', e.srcElement)
-      // console.log('ISE: srcElement', e.toElement)
-      // console.log('ISE: ', e.target.tagName)
       const element = e.target.closest('a')
-      // console.log('ISE: ', element)
-      // if (element !== null && element.tagName.toLowerCase() === 'a') {
-      //   // alert('Caught on click event on an anchor tag !')
-      //   console.log('Found anchor tag: ', element)
-      // }
-
-      // const abortController = new AbortController()
-      // const signal = abortController.signal
       element.addEventListener('click', linkingHandler, { once: true })
-
-      // setTimeout(() => element.removeEventListener('click', linkingHandler), 500)
     }, { capture: true })
   })
-} else {
-  // alert('Window with React Native Webview not available. Plz fix m8 !')
 }
 
 export default bridge
