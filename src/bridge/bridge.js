8c6748b1b23c04614097b4ca03669612dcffa538
Ivan Sekovanikj <Ivan.Sekovanikj@netcetera.com>
2021-07-13 15:33:40 +0200

    Initial implementation

--- src/bridge/bridge.js
+++ src/bridge/bridge.js
@@ -5,6 +5,64 @@ import schemaValidator, {
 
 const v = schemaValidator()
 
+const data_objects = ['data-webview-share']
+
+const findNearestParentWithOnClick = element => {
+  let parent = element
+
+  while (parent !== null && parent.tagName.toLowerCase() !== 'a' && parent.target.dataset.contains(data_objects.some())) {
+    parent = parent.parentNode
+  }
+
+  return parent
+}
+
+const fnc = e => {
+  alert('Found anchor tag ! :D')
+  console.log('ISE: ', e.target)
+  // abortController.abort()
+}
+
+if (window.ReactNativeWebView) {
+  // document.addEventListener('DOMContentLoaded', () => {
+  //   // alert('DOM loaded successfully !')
+  //   const anchors = document.querySelectorAll('a')
+  //   console.log('ISE: ', anchors)
+  //   Array.from(anchors).map(a => a.addEventListener('click', e => {
+  //     alert('Caught on click event !')
+  //     alert(e)
+  //   }))
+  // })
+  document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('click', e => {
+      // alert('A click event has been invoked ! :O')
+      // is anchor
+      // console.log('ISE: event ', e)
+      // console.log('ISE: target ', e.target)
+      // console.log('ISE: eventPhase', e.eventPhase)
+      // console.log('ISE: currentTarget', e.currentTarget)
+      // console.log('ISE: fromElement', e.fromElement)
+      // console.log('ISE: srcElement', e.srcElement)
+      // console.log('ISE: srcElement', e.toElement)
+      // console.log('ISE: ', e.target.tagName)
+      const element = e.target.closest('a')
+      // console.log('ISE: ', element)
+      // if (element !== null && element.tagName.toLowerCase() === 'a') {
+      //   // alert('Caught on click event on an anchor tag !')
+      //   console.log('Found anchor tag: ', element)
+      // }
+
+      // const abortController = new AbortController()
+      // const signal = abortController.signal
+      element.addEventListener('click', fnc)
+
+      setTimeout(() => element.removeEventListener('click', fnc, false), 500)
+    }, { capture: true })
+  })
+} else {
+  // alert('Window with React Native Webview not available. Plz fix m8 !')
+}
+
 const bridge = {}
 
 const fireAction = (action) => {
